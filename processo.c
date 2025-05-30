5324b8d672c08aba8706d7c21f9a0a6d9d2f80df
Caio Santos <106487020+Kaiser-dev-alt@users.noreply.github.com>
2025-05-30 18:54:32 -0300

    Update processo.c

--- processo.c
+++ processo.c
@@ -2,16 +2,16 @@
 #include "simulador.h"
 
 Processo* criar_processo(Simulador *sim, int tamanho_processo) {
+    int i;
     sim->num_processos++;
     sim->processos = realloc(sim->processos, sizeof(Processo) * sim->num_processos);
-
     Processo *p = &sim->processos[sim->num_processos - 1];
     p->pid = sim->num_processos;
     p->tamanho = tamanho_processo;
     p->num_paginas = tamanho_processo / sim->tamanho_pagina;
     p->tabela_paginas = calloc(p->num_paginas, sizeof(Pagina));
 
-    for (int i = 0; i < p->num_paginas; i++) {
+    for (i = 0; i < p->num_paginas; i++) {
         p->tabela_paginas[i].frame = -1;
     }
 
